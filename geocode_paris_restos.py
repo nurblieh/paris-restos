dec6144988174bbe17922b8371c18da7524f3ee7
Bradley Heilbrun <nurblieh@gmail.com>
2012-03-31 08:43:21 +0200

    Filled out geocode_paris_restos.py. Not done tho.

--- geocode_paris_restos.py
+++ geocode_paris_restos.py
@@ -15,8 +15,20 @@ def get_resto_collection():
 
 
 def main():
-  m_paris_restos = get_resto_collection()
+  coll_paris_restos = get_resto_collection()
+  for resto in coll_paris_restos.find():
+    if 'address' not in resto:
+      resto_id = resto._id
+      lat, lon = resto.coordinates['lat'], resto.coordinates['lon']
+      # TODO: do reverse geocode.
+      # example url: http://maps.googleapis.com/maps/geo?q=40.714224,-73.961452&output=json&sensor=true_or_false&key=your_api_key
+      url = 'http://maps.googleapis.com/maps/geo?'
+      url += 'q=%s,%s' % (lat, lon)
+      url += '&output=json&sensor=false'
+      # addr_str = geocode.get(url)
+      print ''%s: %s'' % (resto.name, addr_str)
+      coll_paris_restos.update({'_id': resto_id},
+                               {'$set': {'address': addr_str},
+                               safe=True)
 
-  for resto in m_paris_restos.find():
-    lat, lon = resto.coordinates['lat'], resto.coordinates['lon']
-    
+
