3c05ba10f608952382053fcf224d86b8eec68a14
Bradley Heilbrun <nurblieh@gmail.com>
2012-04-01 07:15:01 +0200

    Working version of the geocoder. Currently stores formatted_address and postal_code.

--- geocode_paris_restos.py
+++ geocode_paris_restos.py
@@ -3,8 +3,10 @@
 # Google Maps API reference on reverse geocoding.
 # https://developers.google.com/maps/documentation/geocoding/#ReverseGeocoding
 
-
 from pymongo import Connection
+import cStringIO
+import json
+import pycurl
 import time
 
 def get_resto_collection():
@@ -13,22 +15,42 @@ def get_resto_collection():
   m_db = m_conn.db1
   return m_db.paris_restos
 
+def query_maps_api(url):
+  buf = cStringIO.StringIO()
+  c = pycurl.Curl()
+  c.setopt(c.URL, str(url))
+  c.setopt(c.WRITEFUNCTION, buf.write)
+  c.setopt(c.TIMEOUT, 10)
+  c.perform()
+  d = json.loads(buf.getvalue())
+  buf.close()
+  return d
 
 def main():
-  coll_paris_restos = get_resto_collection()
-  for resto in coll_paris_restos.find():
-    if 'address' not in resto:
-      resto_id = resto._id
-      lat, lon = resto.coordinates['lat'], resto.coordinates['lon']
-      # TODO: do reverse geocode.
-      # example url: http://maps.googleapis.com/maps/geo?q=40.714224,-73.961452&output=json&sensor=true_or_false&key=your_api_key
-      url = 'http://maps.googleapis.com/maps/geo?'
-      url += 'q=%s,%s' % (lat, lon)
-      url += '&output=json&sensor=false'
-      # addr_str = geocode.get(url)
-      print ''%s: %s'' % (resto.name, addr_str)
-      coll_paris_restos.update({'_id': resto_id},
-                               {'$set': {'address': addr_str},
-                               safe=True)
+  maps_api_url = ('http://maps.googleapis.com/maps/api/geocode/json?'
+                  'latlng=%s,%s&sensor=false')
+  paris_restos = get_resto_collection()
+  for resto in paris_restos.find():
+    resto_id = resto['_id']
+    lat = resto['coordinates']['lat'] 
+    lon = resto['coordinates']['lon']
+    d = query_maps_api(maps_api_url % (lat, lon))
+    if d['status'] == 'OK':
+      # Take the first address.
+      d = d['results'][0]
+      street_addr = d['formatted_address']
+      for i in d['address_components']:
+        if 'postal_code' in i['types']:
+          postal_code = i['short_name']
+          
+      print '%s: %s' % (resto['name'], street_addr)
+      paris_restos.update({'_id': resto_id},
+                          {'$set': {'address': street_addr,
+                                    'postal_code': postal_code}},
+                          safe=True)
+    time.sleep(5)
 
+  return
 
+if __name__ == '__main__':
+  main()
